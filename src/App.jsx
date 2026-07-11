54e9f74b42112e6ac9f5884b3799c06794681193
Cherlia Fleming <cherlia91@gmail.com>
2026-07-11 08:41:38 -0500

    setting up Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import { AllPosts } from "./components/allPosts.jsx"
+import { Route, Routes } from "react-router-dom"
+import { Login } from "./components/auth/Login.jsx"
+import { Register } from "./components/auth/Register.jsx"
+import { Authorized } from "./views/Authorized.jsx"
+import { ApplicationViews } from "./views/ApplicationViews.jsx"
 
 export const App = () => {
-  return <div>Hello World!
-  <AllPosts />
-  </div>
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route
+        path="*"
+        element={
+          <Authorized>
+            <ApplicationViews />
+          </Authorized>
+        }
+      />
+    </Routes>
+  )
 }
